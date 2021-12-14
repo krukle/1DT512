2982919f5ac42ad40f208d31b2e31efadd5884ac
Christoffer Eid <ce223af@student.lnu.se>
2021-12-14 11:01:13 +0100

    start message and forever loop

---
+++ src/dv512/group13/task1/Pipes.java
@@ -0,0 +1,18 @@
+package dv512.group13.task1;
+
+import java.sql.Time;
+import java.time.LocalTime;
+
+/**
+ * Pipes
+ */
+public class Pipes {
+
+  public static void main(String[] args) {
+    System.out.println("PID " + ProcessHandle.current() + " " + LocalTime.now() + " Process started");
+  
+    while (true) {
+      
+    }
+  }
+}
