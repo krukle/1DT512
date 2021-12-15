074507f51b23cbe28b606e1c7b2c13cffebed12c
Isak Karlsson <ik222ne@student.lnu.se>
2021-12-15 12:49:25 +0100

    Task 3 code

--- src/dv512/group13/task1/Pipes.java
+++ src/dv512/group13/task1/Pipes.java
@@ -3,6 +3,8 @@ package dv512.group13.task1;
 import java.time.LocalTime;
 import java.util.Date;
 
+import dv512.group13.task3.Interaction;
+
 /**
  * Pipes
  */
@@ -10,6 +12,5 @@ public class Pipes {
 
   public static void main(String[] args) {
     System.out.println(ProcessHandle.current() + " " + LocalTime.now() + "Process started");
-  
   }
 }
