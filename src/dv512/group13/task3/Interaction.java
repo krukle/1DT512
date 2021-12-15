588768a8b00d034af26e34112e1c8bc488c73c44
Olof Enström <olof.enstrom@gmail.com>
2021-12-15 15:26:24 +0100

    Fix home directory

--- src/dv512/group13/task3/Interaction.java
+++ src/dv512/group13/task3/Interaction.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 public class Interaction {
 
   public static void main(String[] args) {
-    Path path = Paths.get("/home/isak/test-directory/");
+    Path path = Paths.get(System.getProperty("user.home") + "/test-directory/");
     try {
       Files.createDirectories(path);
   
